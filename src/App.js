8f6966e0cb1041e9e18d4f56e42d1e879d70349e
vin <didekqls93@gmail.com>
2023-03-07 16:00:29 +0900

    feat: Router setting on Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieDetail from "./pages/MovieDetail";
 
 function App() {
   return (
     <div>
-      <div> App</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetail />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
 
-// 유저는 배너를 볼 수 있다.
-// 유저는 가장 인기있는 영화 리스트, 가장 평점 좋은 영화 리스트, 상영 예정작 리스트를 볼 수 있다.
-// 각각의 영화 리스트는 슬라이드 형태로 넘길 수 있다.
-// 유저는 영화에 마우스를 올려두면 영화의 제목과 장르, 평점, 인기도, 청불여부를 볼 수 있다.
-// 유저는 영화 카드를 클릭하면 영화의 상세 정보를 볼 수 있다.
-// 상세 정보에는 포스터, 영화 제목, 평점, 인기도 청불여부, 줄거리 요약, 예산, 날짜, 시간, 리뷰, 예고편, 관련영화 등이 있다.
-// 유저는 영화를 검색할 수 있다.
-// 유저는 영화를 인기도순으로 정렬할 수 있다.
-// 유저는 장르별로 영화를 필터링 할 수 있다.
-// 유저는 영화 날짜별로 필터링 할 수 있다.
+//HomePage, moviePage, movieDtailePage
+
+// HomePage
+// 3개의 섹션이 있다 (Popular, Top Rated, Upcoming)
+// 배너에서 제목, 장르, 점수, 인기도, 청불여부 를 볼 수 있다
+// 배너는 슬라이스로 넘겨 볼 수 있다
+
+//movieDtailePage
+//디테일한 정보를 볼 수 있다
+//(포스터, 영화 제목, 평점, 인기도 청불여부, 줄거리 요약, 예산, 날짜, 시간, 리뷰, 예고편, 관련영화 등이 있다.)
+//트레일러를 볼 수 있다
+//리뷰 확인 가능
+//추천 관련 영상이 하단에 노출
+
+//영화 검색을 할 수 있다.
+//영화 정렬을 할 수 있다
+// 영화를 필터링 할 수 있다.
