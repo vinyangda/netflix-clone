c7f6cbd275fabca89308c7eef82f57bda0cbc1b2
vin <didekqls93@gmail.com>
2023-03-08 15:28:28 +0900

    Error : can not see data

--- src/redux/action/movieAction.js
+++ src/redux/action/movieAction.js
@@ -1,11 +1,19 @@
 import api from "../api";
 
+const API_KEY = process.env.REACT_APP_API_KEY;
 function getMovies() {
   return async (dispatch) => {
     const popularMovieApi = await api.get(
-      `https://api.themoviedb.org/3/movie/popular?api_key=0b19239f3136239b18c1dfc58309a223&language=en-US&page=1`
+      `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
     );
-
+    const topRateApi = api.get(
+      `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+    );
+    const upComingApi = api.get(
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
+    );
+    let data = await Promise.all([popularMovieApi]);
+    console.log(data);
     // let url = 'https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1'
     // let res = await fetch(url)
     // let data = await res.json()
