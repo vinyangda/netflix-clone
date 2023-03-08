207da494fda31b17de9cd9c96dc328743c1b440d
vin <didekqls93@gmail.com>
2023-03-09 00:07:49 +0900

    error : Module not found: Error: Can't resolve 'inspector' in

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,23 @@
+import { url } from "inspector";
 import React from "react";
 
 const Banner = ({ movie }) => {
   console.log("movie???:", movie);
   return (
-    <div>
-      <div>Banner</div>
+    <div
+      style={{
+        backgroundImage:
+          "url(" +
+          `https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/${movie.poster_path}` +
+          ")",
+      }}
+    >
+      Banner
     </div>
   );
 };
 
 export default Banner;
+
+// backgroundImage:url
+// 키 값 : string type으로 설정 되어야 함 해서
