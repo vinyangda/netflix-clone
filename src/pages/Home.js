51cbd3700c5571889f07d6b28afaaaa4a82ff313
vin <didekqls93@gmail.com>
2023-03-10 00:06:57 +0900

    install : carousel install

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Banner from "../components/Banner";
 
+import MovieSlide from "../components/MovieSlide";
+
 import { movieAction } from "../redux/action/movieAction";
 
 const Home = () => {
@@ -25,6 +27,12 @@ const Home = () => {
   return (
     <div>
       {popularMovies.results && <Banner movie={popularMovies.results[0]} />}
+      <h1>Popular Movie</h1>
+      <MovieSlide movies={popularMovies} />
+      <h1>Top Rated Movie</h1>
+      <MovieSlide movies={topRatedMovies} />
+      <h1>Upcoming Movie</h1>
+      <MovieSlide movies={upComingMovies} />
     </div>
   );
 };
