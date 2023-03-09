b9b9e04b96f7304fe2e63cf3cd81930b82a48d6e
vin <didekqls93@gmail.com>
2023-03-09 23:54:26 +0900

    feat : create banner / learn how to do make gradient

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,23 +1,24 @@
-import { url } from "inspector";
 import React from "react";
+import movieReducer from "../redux/reducers/movieReducer";
 
 const Banner = ({ movie }) => {
   console.log("movie???:", movie);
   return (
     <div
+      className="banner-background-img"
       style={{
         backgroundImage:
           "url(" +
-          `https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/${movie.poster_path}` +
+          `https://image.tmdb.org/t/p/w1920_and_h800_multi_faces${movie.poster_path}` +
           ")",
       }}
     >
-      Banner
+      <div className="banenr-info">
+        <h1>{movie.title}</h1>
+        <p>{movie.overview}</p>
+      </div>
     </div>
   );
 };
 
 export default Banner;
-
-// backgroundImage:url
-// 키 값 : string type으로 설정 되어야 함 해서
